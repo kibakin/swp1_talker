b988a2ac860e25769cdddad63588b1340d94abb3
kibakin <kibakin@snu.ac.kr>
2015-10-15 11:32:25 +0000

    messages

--- app/controllers/learn_controller.rb
+++ app/controllers/learn_controller.rb
@@ -1,4 +1,12 @@
 class LearnController < ApplicationController
+  
   def index
   end
+  
+  def create
+    
+    Talk.create(ask: params[:ask], answer: params[:answer])
+    redirect_to '/learn'
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   root 'talker#index'
   resources :talker, except: :index
   resources :learn
+  
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
